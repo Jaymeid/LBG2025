9dbc494f4647c3568c8480cf7a31aed114892cbe
TanishR123 <tanish.rokade20@gmail.com>
2025-07-18 10:06:33 +0100

    Tanish

--- src/routes/Savings/savingspage.tsx
+++ src/routes/Savings/savingspage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import exampleImg from '../../assets/PiggyBank.png'
+import savingspicture from '../../assets/Childrens savings.jpg'
 import './Savings.css'; 
 import { HorizontalFlexBox } from '../../components/horizontalFlexBox/HorizontalFlexBox.styled';
 import { Main } from '../../components/styled/Main.styled';
@@ -72,8 +73,11 @@ const Savings: React.FC = () => {
       
       
     </div>
+    <VerticalFlexBox gap={180}>
     <img src={exampleImg} style={{width: 200}}/>
-    <VerticalFlexBox gap={50}>
+        <img src={savingspicture} style={{width: 300, borderRadius: 25}}/>
+    </VerticalFlexBox>
+    <VerticalFlexBox gap={50} style={{marginTop: 20}}>
     <TextBox heading={'Benefits of Saving'} text={'Saving money provides a strong foundation for financial stability and future planning. It allows individuals to prepare for unexpected expenses, achieve personal goals such as buying a home or funding education, and reduce reliance on credit. With a savings account, your money grows through earned interest, all while remaining secure and easily accessible. Regular saving also builds financial discipline, giving you greater control and peace of mind over your long-term financial well-being.'} />
     <TextBox heading={"Children's account"} text={"Start your child’s savings journey with Lloyds Bank. Our Children’s Savings Account helps young savers learn the value of money while earning interest on their savings. It’s a simple, secure way to build healthy financial habits from an early age."} />
 
