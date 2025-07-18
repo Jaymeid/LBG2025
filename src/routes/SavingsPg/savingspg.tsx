9879b06baeec6c9881484caa6144acef5040afe1
AmanieK <amaniekhan2008@outlook.com>
2025-07-18 14:30:58 +0100

    Add text

--- src/routes/SavingsPg/savingspg.tsx
+++ src/routes/SavingsPg/savingspg.tsx
@@ -1,3 +1,28 @@
-export default function SavingsPage() {
-    return <h1> Hello</h1>;
-}
+import TextBox from "../../components/textBox/TextBox";
+import { Main } from "../../components/styled/Main.styled";
+import { HorizontalFlexBox } from "../../components/horizontalFlexBox/HorizontalFlexBox.styled";
+
+export default function savingspg() {
+  return (
+    <Main>
+      <HorizontalFlexBox gap={40}>
+        <TextBox 
+          heading="What is a Savings Account?"
+          text="A savings account is a deposit account held at a financial institution that provides a modest interest rate. It is designed to hold money that is not intended for daily expenses, allowing you to save for future needs while keeping your funds accessible."
+        />
+     
+      <TextBox
+        heading="Benefits of a Savings Account"
+        text="Savings accounts offer several benefits, including earning interest on your deposits, easy access to your funds, and a safe place to store your money. They are ideal for building an emergency fund, saving for specific goals, or simply keeping your money secure."
+      />
+      
+    <TextBox
+       heading="Why Open a Savings Account?"
+       text="A savings account offers a safe place to store your money while earning interest. It's an essential tool for managing your finances, providing liquidity, and helping you reach your financial goals. Furthermore, a savings account is always a smart choice."
+       />
+    </HorizontalFlexBox>
+ 
+      </Main>
+  );
+   
+  }
